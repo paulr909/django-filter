04e1516ba827933313df9afce5da8094542630eb
Paul Rogers <paulrogers909@protonmail.com>
2019-04-13 12:18:30 +0100

    fix author

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,29 +6,14 @@ const Result = ({journal}) => {
     return (
         <div>
             <li>
-        <span>
-          <strong>Title: </strong>{journal.title}{" "}
-        </span>
-                <span>
-          <strong>Author: </strong>{journal.author.name}{" "}
-        </span>
-                <span>
-          <strong>Categories: </strong>
-                    {journal.categories.map(c => {
-                        return <span key={c}>{c} </span>;
-                    })}
-        </span>
-                <span>
-          <strong>Publish date: </strong>{moment(journal.publish_date).format('DD-MM-YYYY')}{" "}
-
-        </span>
-                <span>
-          <strong>View count: </strong>{journal.views}{" "}
-        </span>
-                <span>
-          <strong>Reviewed: </strong>
-                    {`${journal.reviewed}`}{" "}
-        </span>
+                <span><strong>Title: </strong>{journal.title} </span>
+                <span><strong>Author: </strong>{journal.author} </span>
+                <span><strong>Categories: </strong>{journal.categories.map(c => {
+                    return <span key={c}>{c} </span>;
+                })} </span>
+                <span><strong>Publish date: </strong>{moment(journal.publish_date).format('DD-MM-YYYY')} </span>
+                <span><strong>View count: </strong>{journal.views} </span>
+                <span><strong>Reviewed: </strong>{`${journal.reviewed}`} </span>
             </li>
             <hr/>
         </div>
