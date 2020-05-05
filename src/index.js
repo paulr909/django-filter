dea5592623403d4047ad52aba6c07a5f369752ee
Paul Rogers <paulrogers909@protonmail.com>
2020-05-05 18:04:36 +0100

    rm serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 
 const app = <App />;
 
 ReactDOM.render(app, document.getElementById("root"));
-registerServiceWorker();
